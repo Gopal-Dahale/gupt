5cc801ffb10eb6dbe9f80054a58ebdf1843bd99e
Gopal-Dahale <dahalegopal27@gmail.com>
2021-08-21 17:36:16 +0530

    EMNIST + CNN

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gupt
+
+EMNIST downloaded from [here](https://www.nist.gov/itl/products-and-services/emnist-dataset) in matlab zip format.
--- train/training.py
+++ train/training.py
@@ -1,8 +1,10 @@
 """ Training Model"""
 import numpy as np
 import pytorch_lightning as pl
-from gupt.data.mnist_data_module import MNISTDataModule
-from gupt.models.ffnn import FeedForwardNN
+# from gupt.data.mnist_data_module import MNISTDataModule
+from gupt.data.emnist_data_module import EMNISTDataModule
+# from gupt.models.ffnn import FeedForwardNN
+from gupt.models.cnn import CNN
 from gupt import lightning_models
 # from pytorch_lightning import loggers as pl_loggers
 
@@ -11,12 +13,16 @@ def main():
     """Main function to train model
     """
 
-    data = MNISTDataModule(None)
-    data_config = data.config()
+    # data = EMNISTDataModule(None)
+    # data_config = data.config()
+    # model = FeedForwardNN(input_size=np.prod(data_config['input_dims']),
+    #                       hidden_sizes=[1024, 128],
+    #                       output_size=len(data_config['mapping']))
 
-    model = FeedForwardNN(input_size=np.prod(data_config['input_dims']),
-                          hidden_sizes=[1024, 1024],
-                          output_size=len(data_config['mapping']))
+    data = EMNISTDataModule(None)
+    data_config = data.config()
+    model = CNN(input_dims=data_config['input_dims'],
+                mapping=data_config['mapping'])
 
     lit_model = lightning_models.BaseLitModel(model=model, args=None)
 
@@ -25,7 +31,7 @@ def main():
     # logger.watch(model)
 
     trainer = pl.Trainer(
-        max_epochs=2,
+        max_epochs=5,
         weights_save_path='train/logs',
         weights_summary='full',
         #  logger=logger
